ed1d7c51f8258266a3cf7f39ef46de3787a67102
Jazeel Ameen <jazeel.ameen@gmail.com>
2020-08-18 20:14:48 +0530

    Update sketch.js
    refresh

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let v1, v2;
 let buff;
 let c;
-let xoff=0;
+let xoff = 0;
+let p5button;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -9,38 +10,36 @@ function setup() {
   v1 = createVector(buff, buff);
   v2 = createVector(width - buff, height - buff);
   c = createVector(width / 2, height / 2);
-  createButton('p5js sketches').position(width-200,height-200).mousePressed(p5ji);
+  createP("Hi, I'm Jazeel.")
+    .position(v1.x + 10, v1.y - 10)
+    .style("color:white");
+  textAlign(RIGHT);
+  createP("And this is the story of how I -")
+    .position(v2.x - 250, v2.y - 10)
+    .style("color:white;text-align:right;");
+  p5button = createButton("sketches")
+    .position(v1.x, v2.y)
+    .mousePressed(p5buttonpress);
 }
 
-function p5ji(){
-  window.open("https://editor.p5js.org/uhzeel/sketches","_blank");
+function p5buttonpress() {
+  window.open("https://editor.p5js.org/uhzeel/sketches");
 }
 
 function draw() {
-  xoff+=.1;
-  let noiz=50*noise(xoff);
+  xoff += 0.1;
+  let noiz = 50 * noise(xoff);
+
   background(20);
-  
   noFill();
-  
   stroke(100);
   c = createVector(mouseX, mouseY);
   vbezier(v1, c, c, v2);
   stroke(255);
-  c = createVector(mouseX+noiz, mouseY+noiz);
+  c = createVector(mouseX + noiz, mouseY + noiz);
   vbezier(v1, c, c, v2);
-  
-  
-  //line(0,0,mouseX,mouseY); line(width,height,mouseX,mouseY);
-  noStroke();
-  fill(255);
-  textAlign(LEFT);
-  text("this strand is mine.", v1.x + 10, v1.y);
-  textAlign(RIGHT);
-  text("well, alright. we can share it.", v2.x - 10, v2.y);
 }
-function mousePressed() {
-}
+function mousePressed() {}
 
 function vbezier(a, ac, bc, b) {
   bezier(a.x, a.y, ac.x, ac.y, bc.x, bc.y, b.x, b.y);
