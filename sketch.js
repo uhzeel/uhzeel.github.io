f5eaa7067e1830c4196ba7806cb2cb70102fbd22
Jazeel <jazeel.ameen@gmail.com>
2025-01-23 01:51:25 +0530

    fabric in a box

--- sketch.js
+++ sketch.js
@@ -2,12 +2,14 @@ function setup() {
   createCanvas(600, 600, WEBGL);
 }
 
-let reso = 5;
+let reso = 10;
 let zoff = 0;
 let pZ;
 
 function draw() {
+  rotateX(PI/3);
   translate(-width/2,-height/2);
+  
   zoff += 0.005;
   let xoff = 0;
   background(0);
@@ -18,22 +20,10 @@ function draw() {
     for (let j = reso; j < height; j += reso) {
       yoff += 0.01;
       stroke(
-        noise(xoff,yoff,zoff) * 150,
-        map(dist(width/2,height/2,i,j),
-         width/2,0,300,10));
+        noise(xoff,yoff,zoff) * 150)
+        //map(dist(width/2,height/2,i,j),width/2,0,300,10));
       pZ=map(noise(xoff,yoff,zoff),0,1,-450,0);
       point(i,j,pZ);
     }
   }
-  push();
-  translate(width/2,height/2,-80);
-  rotateZ(frameCount*noise(zoff)/100);
-  rotateX(frameCount*noise(zoff)/100);
-  rotateY(frameCount*noise(zoff)/100);
-  noFill();
-  strokeWeight(1);
-  stroke(40);
-  sphere(60,16,16);
-  pop();
-
 }
